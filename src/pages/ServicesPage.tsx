53fc76dbd52c2f12e269085731fefd97c011caf5
Bech, Fadhel (EXTERN: Novobit) <extern.fadhel.bech2@volkswagen.de>
2025-08-28 16:46:20 +0200

    fix build

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,20 +1,16 @@
 // pages/ServicesPage.tsx
 import React from "react";
-import ServicesGrid, { Service } from "./ServicesGrid";
+import ServicesGrid from "./ServicesGrid";
 
 
 
 
 const ServicesPage: React.FC = () => {
-    const handleSelect = (service: Service) => {
-        // Example: route to booking with preselected service
-        // navigate(`/booking?service=${service.id}`);
-        console.log("Selected:", service);
-    };
+
 
     return (
         <main style={{ maxWidth: 1100, margin: "0 auto", padding: "1rem" }}>
-            <ServicesGrid onSelect={handleSelect} />
+            <ServicesGrid />
         </main>
     );
 };
