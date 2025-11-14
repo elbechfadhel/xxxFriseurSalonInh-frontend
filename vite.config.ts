883e20061eed873420f8b882c1f3f85c4b53a61c
Bech, Fadhel (EXTERN: Novobit) <extern.fadhel.bech2@volkswagen.de>
2025-11-14 21:50:13 +0100

    fix page update refresh

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path"
 import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  base: "./",
+  // ⬇️ IMPORTANT CHANGE
+  base: "/",
+  // or simply omit `base` completely, default is "/"
   plugins: [
     react(),
     VitePWA({
